a0e4b778db53298023fe2dcef903217aed1841e7
yun-n <mathpurplee@gmail.com>
2021-03-26 03:18:24 +0900

    136p 6번

--- project.c
+++ project.c
@@ -1,15 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-	double fc = 0;
-	double cc = 0;
+	float kg = 0;
+	float ms = 0;
+	float energy = 0;
 
-	printf("화씨 온도? : ");
-	scanf("%lf", &fc);
-	cc = (fc - 32) * (5.0 / 9.0);
+	printf("질량(kg)? : ");
+	scanf("%f", &kg);
+	printf("속력(m/s)? : ");
+	scanf("%f", &ms);
+	
+	ms = pow(ms,2); // 또는 ms *= ms
+	energy = (1.0 / 2.0) * kg * ms;
 
-	printf("%.lf F = %.2lf", fc, cc);
+	printf("운동에너지 : %.2f J", energy);
 	return 0;
 }
