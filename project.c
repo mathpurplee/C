43a64cef8deb11fac3ec08544ee69b241836b8fc
yun-n <mathpurplee@gmail.com>
2021-03-26 01:47:11 +0900

    두 정수를 입력받아서 계산하는 프로그램

--- project.c
+++ project.c
@@ -3,9 +3,31 @@
 
 int main()
 {
-	int num = 0;
+	float x, y;    // 정수를 입력 받을 변수
+	float z = 0;  // 계산 결과를 저장할 변수
+	char op;    // 연산자를 입력 받을 변수
 
-	printf("10진수 입력 : ");
-	scanf("%d", &num);
-	printf("%x", num);
+	printf("두 개의 정수를 입력하세요 : ");
+	scanf("%f %f", &x, &y);
+	printf("두 정수를 계산할 연산자를 입력하세요 : ");
+	scanf(" %c", &op);
+
+	if (op == '+')
+		z = x + y;
+	else if (op == '-')
+	{
+		if (x >= y)  // 결괏값이 양수가 되도록 if를 중첩해서 정수 크기 비교
+			z = x - y;
+		else if (x < y)
+			z = y - x;
+	}
+	else if (op == '/')
+		z = x / y;
+	else if (op == '*')
+		z = x * y;
+
+	printf("변수 x의 주소 = %d, data = %.f\n", &x, x);
+	printf("변수 y의 주소 = %d, data = %.f\n", &y, y);
+	printf("변수 z의 주소 = %d, data = %.2f\n", &z, z);
+	printf("%.f %c %.f = %.2f\n", x, op, y, z);
 }
