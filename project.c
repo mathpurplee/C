a62f8b50a2c4fd62ba1ce3b944dd1a4b5878619e
yun-n <mathpurplee@gmail.com>
2021-03-26 01:57:09 +0900

    원주율 파이값(수정)

--- project.c
+++ project.c
@@ -8,8 +8,8 @@ int main()
 
 	printf("pi = %.2f\n", pi1);
 	printf("pi = %.4f\n", pi1);
-	printf("pi = %.6f\n", pi1);  // float 유효자릿수 = 7
-	printf("pi = %.8f\n", pi2);  // double 유효자릿수 = 16
+	printf("pi = %.6f\n", pi1);  // float 유효자릿수 7
+	printf("pi = %.8f\n", pi2);  // double 유효자릿수 16
 	printf("pi = %e\n", pi2);
 	return 0;
 
