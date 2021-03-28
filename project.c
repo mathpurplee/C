14a3e4d161420c5e7b610602b831d5a761b1b9a0
yun-n <mathpurplee@gmail.com>
2021-03-28 13:11:52 +0900

    제어문 프로그래밍 과제 1

--- project.c
+++ project.c
@@ -2,13 +2,22 @@
 #include <stdio.h>
 
 int main() {
-	int x1, x2, y1, y2;
-	double m = 0;
+	int score = 0;
 
-	printf("A의 좌표 (x1, y1)? : ");
-	scanf("%d %d", &x1, &y1);
-	printf("B의 좌표 (x2, y2)? : ");
-	scanf("%d %d", &x2, &y2);
-	m = (double)(y2 - y1) / (x2 - x1);
-	printf("직선의 기울기 : %.6lf", m);
+	printf("점수? : ");
+	scanf("%d", &score);
+
+	if (score >= 90 && score <= 100)
+		printf("학점 : A");
+	else if (score >= 80 && score < 90)
+		printf("학점 : B");
+	else if (score >= 70 && score < 80)
+		printf("학점 : C");
+	else if (score >= 60 && score < 70)
+		printf("학점 : D");
+	else if (score >= 0 && score < 60)
+		printf("학점 : F");
+	else
+		printf("다시 입력하세요.");
+	return 0;
 }
