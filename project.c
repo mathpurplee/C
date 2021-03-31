e64439d2861351005dd27e391f5b7397e2ddf9cb
yun-n <mathpurplee@gmail.com>
2021-03-31 15:16:37 +0900

    컴프 지역변수/전역변수

--- project.c
+++ project.c
@@ -1,17 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() {
-	int interger = 0;
-	int num = 0;
+void draw_line(char, int);
+double get_area(double radius);
 
-	printf("양의 정수? : ");
-	scanf("%d", &interger);
-	printf("배수의 개수? : ");
-	scanf("%d", &num);
+int main(void) 
+{
+	int r=0;
+	printf("main()의 지역변수 r의 주소 = %d, data = %d\n", &r, r);
 
-	for (int i = 1; i < num + 1; i++)
-		printf("%d ", interger * i);
+	draw_line('-', 40);
+	for (r = 5; r <= 20; r += 5) {
+		printf("r=%d, 원의 면적:%.2f\n", r, get_area(r));
+	}
+	draw_line('-', 40);
+}
 
-	return 0;
+double get_area(double radius)
+{
+	printf("매개변수 radius의 주소 = %d, data = %lf\n", &radius, radius);
+	const double pi = 3.14;
+	printf("상수 pi의 주소 = %d, data = %f\n", &pi, pi);
+	return pi * radius * radius;
+}
+
+void draw_line(char ch, int len)
+{
+	printf("매개변수 ch의 주소 = %d, data = %c\n", &ch, ch);
+	printf("매개변수 len의 주소 = %d, data = %d\n", &len, len);
+
+	int r=0;
+	printf("draw_line()의 지역변수 r의 주소 = %d, data = %d\n", &r, r);
+
+	for (r = 0; r < len; r++) {
+		printf("%c", ch);
+	}
+	printf("\n");
 }
