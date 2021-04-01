0791a236810a5d973a7c89beb509586e0f403293
yun-n <mathpurplee@gmail.com>
2021-04-01 21:28:45 +0900

    소수 판별 알고리즘(제어문과제3)

--- project.c
+++ project.c
@@ -1,39 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void draw_line(char, int);
-double get_area(double radius);
+int main() {
+	int interger, i;
 
-int main(void) 
-{
-	int r=0;
-	printf("main()의 지역변수 r의 주소 = %d, data = %d\n", &r, r);
+	printf("양의 정수? : ");
+	scanf("%d", &interger);
 
-	draw_line('-', 40);
-	for (r = 5; r <= 20; r += 5) {
-		printf("r=%d, 원의 면적:%.2f\n", r, get_area(r));
+	for (i = 2; i < interger; i++) {
+		if (interger % i == 0)
+			break;
 	}
-	draw_line('-', 40);
+	if (i == interger)
+		printf("%d는 소수입니다.", interger);
+	else
+		printf("%d는 소수가 아닙니다.", interger);
+	return 0;
 }
 
-double get_area(double radius)
-{
-	printf("매개변수 radius의 주소 = %d, data = %lf\n", &radius, radius);
-	const double pi = 3.14;
-	printf("상수 pi의 주소 = %d, data = %f\n", &pi, pi);
-	return pi * radius * radius;
-}
-
-void draw_line(char ch, int len)
-{
-	printf("매개변수 ch의 주소 = %d, data = %c\n", &ch, ch);
-	printf("매개변수 len의 주소 = %d, data = %d\n", &len, len);
-
-	int r=0;
-	printf("draw_line()의 지역변수 r의 주소 = %d, data = %d\n", &r, r);
-
-	for (r = 0; r < len; r++) {
-		printf("%c", ch);
-	}
-	printf("\n");
-}
