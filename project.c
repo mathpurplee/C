bf8b746f3b28f7eaba7b1d6264c0008986fc9e36
yun-n <mathpurplee@gmail.com>
2021-04-07 00:39:51 +0900

    두 점 사이의 거리를 구하는 함수

--- project.c
+++ project.c
@@ -1,26 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-void func(int x, int multiple)
+double distance(double x1, double y1, double x2, double y2)
 {
-	if (x <= 0) {
-		exit(0);
-	}
-	else {
-		for (int i = 1; i <= multiple; i++) {
-			 printf("%d ", x * i);
-		}
-	}
+	return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
 
-int main(void) 
+int main()
 {
-	int interger = 1;
-	while (interger > 0) {
-		printf("정수? : ");
-		scanf("%d", &interger);
-		func(interger, 20);
-		printf("\n");
-	}
+	double x1, x2, y1, y2;
+	printf("직선의 시작점 좌표? : ");
+	scanf("%lf %lf", &x1, &y1);
+	printf("직선의 끝점 좌표? : ");
+	scanf("%lf %lf", &x2, &y2);
+	double res = distance(x1, y1, x2, y2);
+	printf("(%.lf, %.lf)~(%.lf, %.lf) 직선의 길이 : %.6lf", x1, y1, x2, y2, res);
 }
