5099fae242a13ce318bb6588166d6abeb3a7463a
yun-n <mathpurplee@gmail.com>
2021-04-10 19:42:33 +0900

    소수 판별 함수

--- project.c
+++ project.c
@@ -1,25 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-
-int find_leap_year(int year)
-{
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-		return 366;
+int find_prime_num(int num) {
+	int j;
+	for (j = 2; j <= num; j++)
+		if (num % j == 0)
+			break;
+	if (num == j)
+		return num;
 	else
-		return 365;
+		return 0;
 }
-int main()
-{
-	int count = 0;
-	for (int i = 2000; i < 2100; i++) {
-		if (find_leap_year(i) == 366) {
-			printf("%d ", i);
+
+int main() {
+	int interger = 0, count = 0;
+	printf("1~N 사이의 소수를 구합니다. N은? : ");
+	scanf("%d", &interger);
+	for (int i = 2; i <= interger; i++) {
+		if (find_prime_num(i)) {
+			printf("%2d ", i);
 			count++;
 			if (count % 10 == 0)
 				printf("\n");
 		}
 	}
+	return 0;
 }
 
 
