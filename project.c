d053cdb6539ad4f47822b2630f4f48cce340d83b
yun-n <mathpurplee@gmail.com>
2021-04-11 22:05:16 +0900

    16진수 입력을 2진수로 출력

--- project.c
+++ project.c
@@ -1,30 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
-int find_prime_num(int num) {
-	int j;
-	for (j = 2; j <= num; j++)
-		if (num % j == 0)
-			break;
-	if (num == j)
-		return num;
-	else
-		return 0;
+int convert_binary(int x);
+char hex[20];
+
+int main()
+{
+    int input_hex;
+
+    printf("정수? : ");
+    scanf("%x", &input_hex);
+    convert_binary(input_hex);
+
+    return 0;
 }
 
-int main() {
-	int interger = 0, count = 0;
-	printf("1~N 사이의 소수를 구합니다. N은? : ");
-	scanf("%d", &interger);
-	for (int i = 2; i <= interger; i++) {
-		if (find_prime_num(i)) {
-			printf("%2d ", i);
-			count++;
-			if (count % 10 == 0)
-				printf("\n");
-		}
-	}
-	return 0;
+int convert_binary(int x)
+{
+    long ouptput_bin = _itoa(x, hex, 2);
+    printf("16진수 %X : %s", x, ouptput_bin);
 }
-
-
