e770df5838379fed37972a2ea58c51462c110c0f
yun-n <mathpurplee@gmail.com>
2021-04-15 00:50:06 +0900

    이중 배열 & 할인가 계산

--- project.c
+++ project.c
@@ -1,20 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define ARR_SIZE 5
 
-void save_arr(int arr[], int arr_size, int key)
+
+int calculate_discount(int arr[], int index_num, int rate)
 {
-	for (int i = 0; i < arr_size; i++) {
-		arr[i] = key;
-		printf("%d ", key);
-	}
+	int discount_cost = arr[index_num] - (arr[index_num] * ((double)rate / 100));
+	return discount_cost;
 }
 
 int main()
 {
-	int num = 0;
-	int data[20];
+	int discount_rate;                  // 할인율
+	int cost[ARR_SIZE];
+	int discount_cost[ARR_SIZE];
 
-	printf("배열의 원소에 저장할 값? : ");
-	scanf("%d", &num);
-	save_arr(data, 20, num);
+	printf("상품가 5개를 입력하세요 : ");
+	scanf("%d %d %d %d %d", &cost[0], &cost[1], &cost[2], &cost[3], &cost[4]);
+	printf("할인율? : ");
+	scanf("%d", &discount_rate);
+
+	for (int i = 0; i < ARR_SIZE; i++) {
+		discount_cost[i] = calculate_discount(cost, i, discount_rate);
+		printf("가격: %6d --> 할인가: %6d \n", cost[i], discount_cost[i]);
+	}
 }
