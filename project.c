d5e7910fd5713c8f3c2b3c924bcd635618547f8c
yun-n <mathpurplee@gmail.com>
2021-04-14 21:55:26 +0900

    배열 파라미터

--- project.c
+++ project.c
@@ -1,33 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void print_array(int array[], int size)
+void save_arr(int arr[], int arr_size, int key)
 {
-	printf("배열 : ");
-	for (int i = 0; i < size; i++)
-		printf("%d ", array[i]);
-	printf("\n");
+	for (int i = 0; i < arr_size; i++) {
+		arr[i] = key;
+		printf("%d ", key);
+	}
 }
 
 int main()
 {
-	int arr[10] = { 23, 45, 62, 12, 99, 83, 23, 50, 12, 37 };
-	int arr_size = sizeof(arr) / sizeof(arr[0]);
 	int num = 0;
+	int data[20];
 
-	print_array(arr, arr_size);
-
-	printf("찾을 값? : ");
+	printf("배열의 원소에 저장할 값? : ");
 	scanf("%d", &num);
-
-	for (int i = 9; i >= 0; i--) {
-		if (arr[i] == num) {
-			printf("%d는 %d번째 원소입니다.", num, i);
-			break;
-		}
-		else if(arr[i] != num && i<=0)
-			printf("입력한 원소를 찾을 수 없습니다.");
-	}
+	save_arr(data, 20, num);
 }
-
-
