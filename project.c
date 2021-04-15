2e7033da0c3cd34686c939e65eef21d95ff8eff6
yun-n <mathpurplee@gmail.com>
2021-04-16 00:35:08 +0900

    원소 중 최대값, 최소값 찾기

--- project.c
+++ project.c
@@ -1,19 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int find_max(int arr[], int size)
+{
+	int max = 0;  // 최대값을 저장할 변수
+	for (int i = 0; i < size; i++) {
+		if (max < arr[i])
+			max = arr[i];
+	}
+	return max;
+}
+
+int find_min(int arr[], int size)
+{
+	int min = arr[0];   // 최소값을 저장할 변수
+	for (int i = 0; i < size; i++) {
+		if (min >= arr[i])
+			min = arr[i];
+	}
+	return min;
+}
+
 int main()
 {
-	int x = 0, gongcha = 0;
-	int arr[10] = { 0 };
-
-	printf("첫 번째 항? : ");
-	scanf("%d", &x);
-	printf("공차? : ");
-	scanf("%d", &gongcha);
-
-	printf("등차수열 : ");
-	for (int i = 0; i < 10; i++) {
-		arr[i] = x + (i * gongcha);
+	int arr[10] = { 23, 45, 62, 12, 99, 83, 23, 50, 72, 37 };
+	printf("배열 : ");
+	for (int i = 0; i < 10; i++) {   // 배열 출력
 		printf("%d ", arr[i]);
 	}
+	printf("\n");
+	printf("최대값 : %d\n", find_max(arr, 10));
+	printf("최소값 : %d\n", find_min(arr, 10));
+
+
 }
