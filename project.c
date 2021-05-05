4bf66df666ff35215b40a8b5371edac331eee825
yun-n <mathpurplee@gmail.com>
2021-05-06 01:52:49 +0900

    pointer & memory

--- project.c
+++ project.c
@@ -1,24 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define SIZE 5
 
-int get_seconds(int, int, int);
-
-int main(void)
+void print_a(const int* arr1, int size)
 {
-	int hour, min, sec;
+	printf("포인터 arr1의 주소 = %d, data = %d\n", &arr1, arr1);
+	for (int i = 0; i < size; i++) 
+		printf("배열 a[%d]의 주소 = %d, data = %d\n", i, &arr1[i], arr1[i]);
+}
+void print_b(const int* arr2, int size)
+{
+	printf("포인터 arr2의 주소 = %d, data = %d\n", &arr2, arr2);
+	for (int i = 0; i < size; i++)
+		printf("배열 b[%d]의 주소 = %d, data = %d\n", i, &arr2[i], arr2[i]);
+}
+void swap_array(const int *source, int *target, int size);
 
-	printf("시간, 분, 초를 입력 : ");
-	scanf("%d %d %d", &hour, &min, &sec);
-	printf("%d시간 %d분 %d초 = %d초", hour, min, sec, get_seconds(hour, min, sec));
+int main()
+{
+	int a[SIZE] = { 1,3,5,7,9 };
+	int b[SIZE] = { 0,2,4,6,8 };
+	print_a(a, SIZE);
+	print_b(b, SIZE);
+	swap_array(a, b, SIZE);
+	printf("swap_array 호출 후\n");
+	print_a(a, SIZE);
+	print_b(b, SIZE);
 }
 
-int get_seconds(int hour, int min, int sec)
+void swap_array(int *arr1, int *arr2, int size)
 {
-	int result = 0;
-	result += hour * 60 * 60;
-	result += min * 60;
-	result += sec;
-	return result;
-
+	int temp[SIZE];
+	for (int i = 0; i < size; i++) {
+		temp[i] = arr1[i];
+		arr1[i] = arr2[i];
+		arr2[i] = temp[i];
+	}
 }
-
