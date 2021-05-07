f348a308c23718010e84c5f941035a34e5974cbd
yun-n <mathpurplee@gmail.com>
2021-05-07 20:49:37 +0900

    2차원 배열 - pointer

--- project.c
+++ project.c
@@ -1,39 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define SIZE 5
+#define ROW 3
+#define COL 5
 
-void print_a(const int* arr1, int size)
-{
-	printf("포인터 arr1의 주소 = %d, data = %d\n", &arr1, arr1);
-	for (int i = 0; i < size; i++) 
-		printf("배열 a[%d]의 주소 = %d, data = %d\n", i, &arr1[i], arr1[i]);
-}
-void print_b(const int* arr2, int size)
-{
-	printf("포인터 arr2의 주소 = %d, data = %d\n", &arr2, arr2);
-	for (int i = 0; i < size; i++)
-		printf("배열 b[%d]의 주소 = %d, data = %d\n", i, &arr2[i], arr2[i]);
-}
-void swap_array(const int *source, int *target, int size);
+void memory_map(int* p, int row, int val);
 
 int main()
 {
-	int a[SIZE] = { 1,3,5,7,9 };
-	int b[SIZE] = { 0,2,4,6,8 };
-	print_a(a, SIZE);
-	print_b(b, SIZE);
-	swap_array(a, b, SIZE);
-	printf("swap_array 호출 후\n");
-	print_a(a, SIZE);
-	print_b(b, SIZE);
+	int arr[ROW][COL] = { 0 };
+	int val = 0;
+	int row = 0;
+	
+	printf("배열의 원소에 저장할 값? : ");
+	scanf("%d", &val);
+	for (row = 0; row < ROW; row++) {
+		memory_map(arr[row], row, val);
+		printf("\n");
+	}
 }
 
-void swap_array(int *arr1, int *arr2, int size)
+void memory_map(int* p, int row, int val)
 {
-	int temp[SIZE];
-	for (int i = 0; i < size; i++) {
-		temp[i] = arr1[i];
-		arr1[i] = arr2[i];
-		arr2[i] = temp[i];
+	printf("포인터 p의 주소 = %d, data = %d\n", &p, p);
+	for (int col = 0; col < COL; col++) {
+	*(p+col) = val;
+		printf("배열 arr[%d][%d]의 주소 = %d, data = %d\n", row, col, &p[col], *(p+col));
 	}
 }
