f2deaba55cc6f7a9faf93f565a83a5c4e9dd5ba4
yun-n <mathpurplee@gmail.com>
2021-05-16 12:58:57 +0900

    문자열 reverse & 문자열 포인터

--- project.c
+++ project.c
@@ -1,42 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+
+void reverse_string(char* str)
+{
+	char temp = "";
+	printf("문자 변수 temp의 주소 = %d, data = %c\n", &temp, temp);
+	printf("포인터 변수 str의 주소 = %d, data = %d\n", &str, str);
+	for (int i = 0, j = strlen(str) - 1; i < j; i++, j--) {
+		printf("(바꾸기 전)str[%2d]의 주소 = %d, data = %c / str[%2d]의 주소 = %d, data = %c / temp의 data = %c\n", i, &str[i], str[i], j, &str[j], str[j], temp);
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+		printf("(바꾸기 후)str[%2d]의 주소 = %d, data = %c / str[%2d]의 주소 = %d, data = %c / temp의 data = %c\n", i, &str[i], str[i], j, &str[j], str[j], temp);
+	}
+}
 
 int main()
 {
-	char FileName[20] = "";
-	char FileExtension[5] = "";
-	char File[30] = "";
+	char s[15] = "";
+	for (int i = 0; i < 15; i++)
+		printf("문자열배열 s[%2d]의 주소 = %d, data = %c\n", i, &s[i], s[i]);
 
-	for (int i = 0; i < 20; i++)
-		printf("FileName[%2d]의 주소 = %d, data = %c\n", i, &FileName[i], FileName[i]);
-	printf("\n");
-	for (int i = 0; i < 5; i++)
-		printf("FileExtension[%d]의 주소 = %d, data = %c\n", i, &FileExtension[i], FileExtension[i]);
-	printf("\n");
-	for (int i = 0; i < 20; i++)
-		printf("File[%2d]의 주소 = %d, data = %c\n", i, &File[i], File[i]);
-	printf("\n");
+	printf("문자열? ");
+	gets(s);
 
-	printf("파일명? ");
-	gets(FileName);
-	printf("확장자? ");
-	gets(FileExtension);
-
-	strcpy(File, FileName);
-	strcat(File, ".");
-	strcat(File, FileExtension);
-
-	printf("전체 파일명 : %s\n", File);
-
-	for (int i = 0; i < 20; i++)
-		printf("FileName[%2d]의 주소 = %d, data = %c\n", i, &FileName[i], FileName[i]);
-	printf("\n");
-	for (int i = 0; i < 5; i++)
-		printf("FileExtension[%d]의 주소 = %d, data = %c\n", i, &FileExtension[i], FileExtension[i]);
-	printf("\n");
-	for (int i = 0; i < 20; i++)
-		printf("File[%2d]의 주소 = %d, data = %c\n", i, &File[i], File[i]);
-	printf("\n");
+	reverse_string(s); 
+    printf("역순으로 된 문자열 : %s\n", s);
+	for (int i = 0; i < 15; i++)
+		printf("문자열배열 s[%2d]의 주소 = %d, data = %c\n", i, &s[i], s[i]);
 }
