ade5f964f0c2484709744f168a295d8b93771c9e
yun-n <mathpurplee@gmail.com>
2021-05-17 13:31:51 +0900

    ctype.h - isalpha() 이용

--- project.c
+++ project.c
@@ -1,32 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-
-void reverse_string(char* str)
-{
-	char temp = "";
-	printf("문자 변수 temp의 주소 = %d, data = %c\n", &temp, temp);
-	printf("포인터 변수 str의 주소 = %d, data = %d\n", &str, str);
-	for (int i = 0, j = strlen(str) - 1; i < j; i++, j--) {
-		printf("(바꾸기 전)str[%2d]의 주소 = %d, data = %c / str[%2d]의 주소 = %d, data = %c / temp의 data = %c\n", i, &str[i], str[i], j, &str[j], str[j], temp);
-		temp = str[i];
-		str[i] = str[j];
-		str[j] = temp;
-		printf("(바꾸기 후)str[%2d]의 주소 = %d, data = %c / str[%2d]의 주소 = %d, data = %c / temp의 data = %c\n", i, &str[i], str[i], j, &str[j], str[j], temp);
-	}
-}
+#include <ctype.h>
 
 int main()
 {
-	char s[15] = "";
-	for (int i = 0; i < 15; i++)
-		printf("문자열배열 s[%2d]의 주소 = %d, data = %c\n", i, &s[i], s[i]);
+	char str[70] = "";
+	int NumOfAlpha = 0;
+
+	for (int i = 0; i < 70; i++)       //     문자열 입력 전 memory 출력
+		printf("str[%2d]의 주소 = %d, data = %c\n", i, &str[i], str[i]);
+	printf("변수 NumOfAlpha의 주소 = %d, data = %d\n", &NumOfAlpha, NumOfAlpha);
 
 	printf("문자열? ");
-	gets(s);
+	gets(str);
 
-	reverse_string(s); 
-    printf("역순으로 된 문자열 : %s\n", s);
-	for (int i = 0; i < 15; i++)
-		printf("문자열배열 s[%2d]의 주소 = %d, data = %c\n", i, &s[i], s[i]);
+	for (int i = 0; i < strlen(str); i++) {
+		if (isalpha(str[i]))
+			NumOfAlpha++;
+	}
+	printf("영문자의 개수 : %d\n", NumOfAlpha);
+
+	for (int i = 0; i < 70; i++)       //     문자열 입력 후 memory 출력
+		printf("str[%2d]의 주소 = %d, data = %c\n", i, &str[i], str[i]);
+	printf("변수 NumOfAlpha의 주소 = %d, data = %d\n", &NumOfAlpha, NumOfAlpha);
 }
