88e59d52db88aca2fc14b0432fd6d1272f3d4108
yun-n <mathpurplee@gmail.com>
2021-05-24 21:57:49 +0900

    구조체안에 열거체 & Memory

--- project.c
+++ project.c
@@ -1,24 +1,54 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define SONG_SIZE 8
+#define GENRE_NAME_SIZE 8
 #include <stdio.h>
 #include <string.h>
 
-struct point {
-	int x, y;
+enum Genre {
+	ballad, hiphop, soul, dance, pop
 };
-struct rect {
-	struct point left_bottom, right_bottom;
+struct song {
+	char title[20];
+	char artist[16];
+	enum Genre genre;
 };
-print_rect_info(const struct rect* r)
-{
-	printf("[RECT 좌하단점 : (%d, %d) 우상단점 : (%d, %d)]", r->left_bottom.x, r->left_bottom.y, r->right_bottom.x, r->right_bottom.y);
-}
-
 int main()
 {
-	struct rect r = { 0 };
-	printf("직사각형의 좌하단점(x, y)? ");
-	scanf("%d %d", &r.left_bottom.x, &r.left_bottom.y);
-	printf("직사각형의 우상단점(x, y)? ");
-	scanf("%d %d", &r.right_bottom.x, &r.right_bottom.y);
-	print_rect_info(&r);
+	char genre_name[GENRE_NAME_SIZE] = { 0 };
+	struct song songs[SONG_SIZE] = {
+		{"밤편지", "아이유", ballad},
+		{"I'm The One", "DJ Khaled", hiphop},
+		{"Jealous", "DJ Khaled", hiphop},
+		{"한여름밤의 꿀", "San E", hiphop},
+		{"서울 밤", "어반자카파", soul},
+		{"썸머", "GRAY", soul},
+		{"누구 없소(No One)", "이하이", dance},
+		{"Sixteen", "Ellie Goulding", pop},
+	};
+
+	printf("%s %41s %29s", "제목", "아티스트", "장르");
+	for (int i = 0; i < SONG_SIZE; i++) {
+		switch (songs[i].genre) {
+		case ballad:
+			strcpy(genre_name, "ballad");
+			break;
+		case hiphop:
+			strcpy(genre_name, "hiphop");
+			break;
+		case soul:
+			strcpy(genre_name, "soul");
+			break;
+		case dance:
+			strcpy(genre_name, "dance");
+			break;
+		case pop:
+			strcpy(genre_name, "pop");
+			break;
+		}
+		printf("\nsongs[%d]의 주소 = %8d\n", i, &songs[i]);
+		printf("songs[%d].title의 주소 = %d      songs[%d].artist의 주소 = %d songs[%d].genre의 주소 = %d\n", i, &songs[i].title, i, &songs[i].artist, i, &songs[i].genre);
+		printf("songs[%d].title = %-20s songs[%d].artist = %-15s songs[%d].genre = %d\n", i, songs[i].title, i, songs[i].artist, i, songs[i].genre);
+		for (int i = 0; i < GENRE_NAME_SIZE; i++)
+			printf("genre_name[%d]의 주소 = %d, data = %c\n", i, &genre_name[i], genre_name[i]);
+	}
 }
