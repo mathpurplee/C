8befd8c2a6559fd61314f2fc0048041297e9d480
yun-n <mathpurplee@gmail.com>
2021-06-09 23:59:59 +0900

    토큰&포인터

--- project.c
+++ project.c
@@ -1,48 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-#define MAX 100
-#define STR_SIZE 10
+int main(void) {
+	char phone[] = "010-123-4567";
+	char* p = NULL;
 
-struct employee {
-	char name[STR_SIZE];
-	char depart[STR_SIZE];
-	int salary;
-};
+	printf("배열 phone[]의 주소 = %d, data = %s\n", phone, phone);  // 배열의 이름은 배열의 시작 주소 
+	for (int i = 0; i < strlen(phone); i++) 
+		printf("phone[%2d]의 주소 = %d, data = %c\n", i, &phone[i], phone[i]);
+	printf("\n");
 
-int main(void)
-{
-	struct employee* group[MAX] = { NULL };
-	int i;
-	int cnt = 0;
-	while (cnt < MAX) {
-		char temp[STR_SIZE] = "";                // 임시 버퍼
-		printf("\n등록할 직원 이름? ");
-		gets(temp);
-		if (strcmp(temp, ".") == 0)
-			break;
-		group[cnt] = (struct employee*)malloc(sizeof(struct employee));
-		strcpy(group[cnt]->name, temp);
-		printf("<<포인터 group[%d]의 주소 = %d, data = %d>>\n", cnt, &group[cnt], group[cnt]);
-		printf("<group[%d]->name의 주소 = %d, data = %s>\n", cnt, &group[cnt]->name, group[cnt]->name);
+	p = strtok(phone, "-"); // 첫 번째 토큰 p[3] = "-"의 주소 리턴
+	printf("포인터 변수 p의 주소 = %d, data = %d,  NULL확인 p[3] = %s\n", &p, p, p[3]);
 
-		printf("부서명? ");
-		gets(group[cnt]->depart);
-		printf("<group[%d]->depart의 주소 = %d, data = %s>\n", cnt, &group[cnt]->depart, group[cnt]->depart);
+	p = strtok(NULL, "-"); // 두 번째 토큰 p[7] = "-"의 주소 리턴
+	printf("포인터 변수 p의 주소 = %d, data = %d,  NULL확인 p[3] = %s\n", &p, p, p[3]);
 
-		printf("연봉(만원)? ");
-		scanf("%d", &group[cnt]->salary);
-		printf("<group[%d]->salary의 주소 = %d, data = %d>\n", cnt, &group[cnt]->salary, group[cnt]->salary);
-		cnt++;
-		while (getchar() != '\n') {}
-	}
-	printf("%\n%d명의 직원을 등록했습니다.\n", cnt);
-	for (i = 0; i < cnt; i++)
-		printf("%s / %s / %d만원\n", group[i]->name, group[i]->depart, group[i]->salary);
-	for (i = 0; i < cnt; i++) {
-		free(group[i]);
-		group[i] = NULL;
-	}
+	p = strtok(NULL, "-"); // 두 번째 토큰 p[12]= "-"의 주소 리턴
+	printf("포인터 변수 p의 주소 = %d, data = %d,  NULL확인 p[4] = %s\n", &p, p, p[4]);
 }
